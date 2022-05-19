openwrt/gh-action-sdk

commit 653d0c2f5cdceb771fca345f609a47e92010ada5

Don't build packages as root
Signed-off-by: Jeffery To <jeffery.to@gmail.com>

action.yml
@@ -4,6 +4,10 @@ author: aparcar
 runs:
   using: 'composite'
   steps:
+    - run: mkdir $GITHUB_WORKSPACE/artifacts
+      shell: bash
+    - run: chmod o+rw $GITHUB_WORKSPACE/artifacts
+      shell: bash
     - run: docker build --build-arg CONTAINER --build-arg ARCH -t sdk $GITHUB_ACTION_PATH
       shell: bash
     - run: |
@@ -23,4 +27,8 @@ runs:
           -v $(pwd):/github/workspace \
           -v $GITHUB_WORKSPACE:$GITHUB_WORKSPACE \
           sdk
+      shell: bash
+    - run: cp -r $GITHUB_WORKSPACE/artifacts/. $GITHUB_WORKSPACE/
+      shell: bash
+    - run: sudo rm -rf $GITHUB_WORKSPACE/artifacts/
       shell: bash

entrypoint.sh
@@ -125,9 +125,9 @@ else
 fi
 
 if [ -d bin/ ]; then
-	mv bin/ "$GITHUB_WORKSPACE/"
+	mv bin/ "$GITHUB_WORKSPACE/artifacts/"
 fi
 
 if [ -d logs/ ]; then
-	mv logs/ "$GITHUB_WORKSPACE/"
+	mv logs/ "$GITHUB_WORKSPACE/artifacts/"
 fi

Dockerfile
@@ -6,6 +6,4 @@ LABEL "com.github.actions.name"="OpenWrt SDK"
 
 ADD entrypoint.sh /
 
-USER root
-
 ENTRYPOINT ["/entrypoint.sh"]

jefferyto/openwrt-gh-action-sdk

commit 092cc9305846300ed56d10b09bb08af158ac51b8

Fix errors when building packages with patches
Currently, the refresh command within the test build will fail because
the user inside the sdk image (uid 1000, gid 1000) does not have
permission to overwrite patches in the feed directory
($GITHUB_WORKSPACE, bind mounted from the action container.)

This changes the ownership of $GITHUB_WORKSPACE to the sdk image user
before the build, then restores the original owner after the build.

Fixes: 653d0c2f5cdc ("Don't build packages as root")
Signed-off-by: Jeffery To <jeffery.to@gmail.com>

action.yml
@@ -4,9 +4,7 @@ author: aparcar
 runs:
   using: 'composite'
   steps:
-    - run: mkdir $GITHUB_WORKSPACE/artifacts
-      shell: bash
-    - run: chmod o+rw $GITHUB_WORKSPACE/artifacts
+    - run: sudo chown -R 1000:1000 $GITHUB_WORKSPACE
       shell: bash
     - run: docker build --build-arg CONTAINER --build-arg ARCH -t sdk $GITHUB_ACTION_PATH
       shell: bash
@@ -28,7 +26,5 @@ runs:
           -v $GITHUB_WORKSPACE:$GITHUB_WORKSPACE \
           sdk
       shell: bash
-    - run: cp -r $GITHUB_WORKSPACE/artifacts/. $GITHUB_WORKSPACE/
-      shell: bash
-    - run: sudo rm -rf $GITHUB_WORKSPACE/artifacts/
+    - run: sudo chown -R --reference=$GITHUB_WORKSPACE/.. $GITHUB_WORKSPACE
       shell: bash

entrypoint.sh
@@ -125,9 +125,9 @@ else
 fi
 
 if [ -d bin/ ]; then
-	mv bin/ "$GITHUB_WORKSPACE/artifacts/"
+	mv bin/ "$GITHUB_WORKSPACE/"
 fi
 
 if [ -d logs/ ]; then
-	mv logs/ "$GITHUB_WORKSPACE/artifacts/"
+	mv logs/ "$GITHUB_WORKSPACE/"
 fi